6a6808505af59918816b0790a8517e9514756c47
Cliff L. Biffle <code@cliffle.com>
2016-10-28 13:22:03 -0700

    vm-bootstrap: update GCC version from PPA

--- vm-bootstrap.sh
+++ vm-bootstrap.sh
@@ -5,7 +5,7 @@ sudo apt-get install -y software-properties-common
 sudo add-apt-repository ppa:team-gcc-arm-embedded/ppa
 sudo apt-get update
 sudo apt-get install -y \
-  gcc-arm-embedded=5-2016q2-1~trusty1 \
+  gcc-arm-embedded=5-2016q3-1~trusty1 \
   build-essential \
   git \
   ninja-build
