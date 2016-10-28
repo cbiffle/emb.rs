b3a8e1f0533f2064a28a779ea9244516111f1dd2
Cliff L. Biffle <code@cliffle.com>
2016-10-28 13:22:47 -0700

    Start using embrs from main.

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,12 @@
-#![feature(lang_items,start)]
+#![feature(const_fn)]
+#![feature(lang_items)]
+
 #![no_std]
 #![no_main]
+
+extern crate embrs;
+
+use embrs::arm_m::exc;
 
 /******************************************************************************/
 
@@ -25,80 +31,6 @@ extern {
     /// stack pointer.
     static __STACK_BASE: u32;
 }
-
-/// ARMvx-M interrupt and exception handlers are merely functions conforming to
-/// the C ABI.
-pub type Handler = extern "C" fn();
-
-/// Represents an ARMvx-M exception table.  This is the common table of vectors
-/// used for handling interrupts and initializing the processor on -M
-/// processors.
-///
-/// Vectors are described using the `Handler` type, which is merely a function
-/// reference.  Most vectors in the table are modeled as `Option<Handler>`,
-/// relying on the null pointer optimization to store zero for `None`.
-/// Applications that don't use a particular vector can thus omit it.
-///
-/// Note that processors will typically have a *two-part* vector table: first
-/// come the exception vectors (described here), immediately followed by
-/// vendor-specific interrupt vectors handled through the NVIC.  We model such
-/// tables in two parts, and concatenate them at link time.  Thus, you will not
-/// find vendor-specific vectors here.
-#[repr(C, packed)]
-pub struct ExceptionTable {
-    /// ARMvx-M processors load their initial stack pointer from the first word
-    /// of the vector table.  This will be the contents of `sp` on entry to
-    /// `reset` below.
-    ///
-    /// Remember that ARM uses a "full descending" stack, so `sp` points to the
-    /// most recently *used* cell of the stack.  Thus, the initial `sp` when the
-    /// stack is empty often points just past the end of RAM.  We model it here
-    /// as a `const` pointer to discourage such an invalid address from being
-    /// dereferenced.
-    pub initial_stack: *const u32,
-
-    /// Reset vector.  At reset, the processor loads its stack pointer from
-    /// `initial_stack` (above) and then enters this function using the ARM
-    /// AAPCS C ABI.
-    ///
-    /// If the function implementing the reset vector returns, we get
-    /// architecturally undefined behavior.  We can conveniently model this in
-    /// Rust by describing the function as diverging.
-    pub reset: unsafe extern "C" fn() -> !,
-
-    // Architecturally defined exception vectors (i.e. those that are
-    // vendor-independent) begin here.  The architectural vector table includes
-    // five reserved entries.  We model them here as `pub` because certain
-    // naughty vendors (looking at you, NXP) have a loose interpretation of the
-    // term "reserved" and stuff things into them anyway.
-
-    pub nmi:          Option<Handler>,
-    pub hard_fault:   Option<Handler>,
-    pub mm_fault:     Option<Handler>,
-    pub bus_fault:    Option<Handler>,
-    pub usage_fault:  Option<Handler>,
-    pub _reserved0:   Option<Handler>,
-    pub _reserved1:   Option<Handler>,
-    pub _reserved2:   Option<Handler>,
-    pub _reserved3:   Option<Handler>,
-    pub sv_call:      Option<Handler>,
-    pub debug_mon:    Option<Handler>,
-    pub _reserved4:   Option<Handler>,
-    pub pend_sv:      Option<Handler>,
-    pub sys_tick:     Option<Handler>,
-}
-
-/// Most programs will have at least one `ExceptionTable` `static`: the one that
-/// gets deposited into ROM and read at processor startup.
-///
-/// To support a `static` `ExceptionTable`, the type must be `Sync`.  It is
-/// *almost* `Sync` out of the box.  The exception: the pointer used for the
-/// `initial_stack` item.
-///
-/// To hack around this, we stamp `ExceptionTable` as `Sync`.  This is probably
-/// not the right solution.
-unsafe impl Sync for ExceptionTable {}
-
 
 /******************************************************************************/
 
@@ -133,22 +65,17 @@ extern "C" fn trap() { loop {} }
 ///    this symbol is left visible to the linker.
 #[no_mangle]
 #[link_section=".isr_vector"]
-pub static ISR_VECTORS : ExceptionTable = ExceptionTable {
-    initial_stack: unsafe { &__STACK_BASE },  // unsafe ref-to-ptr conversion
-    reset: reset_handler,
-
+pub static ISR_VECTORS : exc::ExceptionTable = exc::ExceptionTable {
     nmi: Some(trap),
     hard_fault: Some(trap),
     mm_fault: Some(trap),
     bus_fault: Some(trap),
     usage_fault: Some(trap),
-    _reserved0: None,
-    _reserved1: None,
-    _reserved2: None,
-    _reserved3: None,
     sv_call: Some(trap),
     debug_mon: Some(trap),
-    _reserved4: None,
     pend_sv: Some(trap),
     sys_tick: Some(trap),
+
+    .. exc::empty_exception_table(unsafe { &__STACK_BASE },
+                                  reset_handler)
 };
