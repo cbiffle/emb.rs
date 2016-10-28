2fe385124102d91423db72f3e99747946912c194
Cliff L. Biffle <code@cliffle.com>
2016-10-28 10:16:18 -0700

    arm_m: support for PRIMASK, ISB

--- embrs/src/arm_m/mod.rs
+++ embrs/src/arm_m/mod.rs
@@ -1,1 +1,20 @@
 pub mod reg;
+
+/// Sets the processor's `PRIMASK` register to `val`.
+#[inline]
+pub fn set_primask(val: bool) {
+    unsafe {
+        asm!("msr PRIMASK, $0"
+             :: "r"(val)
+             :: "volatile")
+    }
+}
+
+/// Generates an instruction synchronization barrier (`ISB`) instruction.  For
+/// other types of barriers, see Rust's fence operations.
+#[inline]
+pub fn instruction_synchronization_barrier() {
+    unsafe {
+        asm!("isb" :::: "volatile")
+    }
+}
