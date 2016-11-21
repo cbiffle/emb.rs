cb6ebac650b6d87e156e7104182dd50a6c787fc8
Cliff L. Biffle <code@cliffle.com>
2016-11-21 08:49:57 -0800

    Send LED status on USART2 @ 115200.

--- src/main.rs
+++ src/main.rs
@@ -10,8 +10,8 @@
 extern crate embrs;
 
 use embrs::arm_m::{self, exc, sys_tick};
-use embrs::stm32f4::rcc::{self, RCC, AhbPeripheral};
-use embrs::stm32f4::gpio::{self, GPIOD};
+use embrs::stm32f4::rcc::{self, RCC, AhbPeripheral, ApbPeripheral};
+use embrs::stm32f4::gpio::{self, GPIOA, GPIOD};
 
 /******************************************************************************/
 
@@ -24,32 +24,30 @@ fn led_pins() -> gpio::PinMask {
 
 /// Frequency of toggling (= half frequency of blinking).
 const TOGGLE_HZ : u32 = 10;
+const HZ : u32 = 160_000_000;
+
+const CLOCKS : rcc::ClockConfig = rcc::ClockConfig {
+    crystal_hz: 8_000_000_f32,
+    crystal_divisor: 4,
+    vco_multiplier: 160,
+    general_divisor: rcc::SysPrescaler::Div2,
+    pll48_divisor: 4,
+
+    ahb_divisor: None,
+    apb1_divisor: Some(rcc::ApbPrescaler::Div4),
+    apb2_divisor: Some(rcc::ApbPrescaler::Div2),
+
+    flash_latency: 5,
+};
+
 
 /// The application entry point.  This name isn't special, it's called from
 /// `reset_handler` below.  Note that we're in safe code at this point.
 fn app() -> ! {
-    RCC.configure_clocks(&rcc::ClockConfig {
-        crystal_hz: 8_000_000_f32,
-        crystal_divisor: 4,
-        vco_multiplier: 160,
-        general_divisor: rcc::SysPrescaler::Div2,
-        pll48_divisor: 4,
+    RCC.configure_clocks(&CLOCKS);
 
-        ahb_divisor: None,
-        apb1_divisor: Some(rcc::ApbPrescaler::Div4),
-        apb2_divisor: Some(rcc::ApbPrescaler::Div2),
-
-        flash_latency: 5,
-    });
-
-    const HZ : u32 = 160_000_000;
-
-    // Enable clock to GPIOD so we can mess with its registers.
-    RCC.enable_clock(AhbPeripheral::GpioD);
-
-    // Configure our pins for push-pull digital output.
-    GPIOD.set_mode(led_pins(), gpio::Mode::Gpio);
-    GPIOD.set_output_type(led_pins(), gpio::OutputType::PushPull);
+    init_leds();
+    init_uart();
 
     // Configure the SysTick timer to generate interrupts at our toggle
     // frequency.
@@ -68,11 +66,48 @@ fn app() -> ! {
     }
 }
 
+fn init_leds() {
+    // Enable clock to GPIOD so we can mess with its registers.
+    RCC.enable_clock(AhbPeripheral::GpioD);
+
+    // Configure our pins for push-pull digital output.
+    GPIOD.set_mode(led_pins(), gpio::Mode::Gpio);
+    GPIOD.set_output_type(led_pins(), gpio::OutputType::PushPull);
+}
+
+fn init_uart() {
+    use embrs::stm32f4::usart::*;
+
+    // Enable clock to USART2.
+    RCC.enable_clock(ApbPeripheral::Usart2);
+
+    USART2.update_cr1(|v| v.with_ue(true));
+
+    let speeds = CLOCKS.compute_speeds();
+
+    let clk = speeds.get_clock_for(ApbPeripheral::Usart2);
+    let brr = (clk / 115200_f32 + 0.5) as u32;
+
+    USART2.update_brr(|v| v.with_mantissa(brr >> 4)
+                      .with_fraction(brr & 0xF));
+
+    USART2.update_cr1(|v| v.with_te(true));
+
+    RCC.enable_clock(AhbPeripheral::GpioA);
+    // Configure its TX pin (PA2) as AF7
+    GPIOA.set_alternate_function(gpio::P2, gpio::Function::AF7);
+    GPIOA.set_mode(gpio::P2, gpio::Mode::Alternate);
+}
+
 /// Interrupt handler that toggles our LEDs.
 extern "C" fn toggle_isr() {
+    use embrs::stm32f4::usart::*;
+
     if GPIOD.get(led_pins()).is_empty() {
+        USART2.send8(b'1');
         GPIOD.set(led_pins())
     } else {
+        USART2.send8(b'0');
         GPIOD.clear(led_pins())
     }
 }
